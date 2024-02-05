1b810f350bc4db47a06e38573a547ae050b2faf6
Avinash <avin.roy001@gmail.com>
2024-02-05 15:56:02 +0530

    error correction commit

--- law-firm/src/components/MailBox/MailBox.jsx
+++ law-firm/src/components/MailBox/MailBox.jsx
@@ -4,9 +4,10 @@ import { IoIosMail } from "react-icons/io";
 const MailBox = () => {
   return (
     <div className={styles.mailbox}>
-      <div>
-        <IoIosMail />
-        <input placeholder='Enter your email address'></input>
+      <div className={styles.inputblock}>
+        <IoIosMail  size={30} color='var(--color-primary)'/>
+        <input placeholder='Enter your email address'>
+        </input>
       </div>
       <div className={styles.button}>
         <button>Let's Talk</button>
